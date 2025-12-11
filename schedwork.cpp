daaf05db6eadcf0855bc66a07eaf4b72e0fe7452
Evelyn Gonzalez <evelynmg@usc.edu>
2025-12-11 05:24:18 +0000

    done with wordle/sched

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,6 +21,21 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool schedule_help (
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    size_t day, 
+    size_t slots
+);
+
+bool schedule_worker(
+    Worker_T worker,
+    const DailySchedule& sched,
+    size_t maxShift, 
+    size_t currDay
+);
 
 
 // Add your implementation of schedule() and other helper functions here
@@ -38,8 +53,76 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+    size_t n = avail.size();
+    sched.resize(n);
+
+    return schedule_help(avail, dailyNeed, maxShifts, sched, 0, 0);
+
+}
+
+bool schedule_help (
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    size_t day, 
+    size_t slots
+)
+{
+
+  // number of workers 
+  // and number of employess 
+  size_t n = avail.size();
+  size_t k = avail[0].size(); 
 
 
+  if (day == n){
+    return true;
+  }
 
+  // if the day is full, do a recurisve  call
+  if (slots == dailyNeed){
+    return schedule_help(avail, dailyNeed, maxShifts, sched, day +1, 0);
+  }
+
+  //  going through every worker to see which  will work
+  for (Worker_T worker = 0; worker < k; worker++){
+    // chekc to see if the worker is aviable, hsa reached the max amount of maxShifts
+    // also will check if they were alreayd schedlued for the day we are searchign for
+    if (avail[day][worker] && schedule_worker(worker, sched, maxShifts, day) && find(sched[day].begin(), sched[day].end(), worker) == sched[day].end()){
+      
+      sched[day].push_back(worker);
+
+      // rescurive call
+      if(schedule_help(avail, dailyNeed, maxShifts, sched, day, slots +1)){
+        return true;
+      }
+
+      sched[day].pop_back();
+    }
+
+  }
+
+  return false;
+}
+
+
+bool schedule_worker(
+    Worker_T worker,
+    const DailySchedule& sched,
+    size_t maxShift, 
+    size_t currDay
+)
+{
+  size_t count =0;
+
+  //checks to see how mnay shift they have
+  for (size_t day =0; day < currDay; day++){
+    if (find(sched[day].begin(), sched[day].end(), worker) != sched[day].end()){
+      count++;
+    }
+  }
+
+  return count < maxShift;
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -2,7 +2,7 @@
 // For debugging
 #include <iostream>
 // For std::remove
-#include <algorithm> 
+//#include <algorithm> 
 #include <map>
 #include <set>
 #endif
@@ -13,7 +13,13 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void wordle_help(
+    int index,
+    std::string& current,
+    const std::string& in,
+    std::string& float_left,
+    const std::set<std::string>& dict,
+    std::set<std::string>& result);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +28,113 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    set<string> result;
+    // if ( in.empty()){
+    //   return result;//no input 
+    // }
+    string current = in;
+
+    string float_copy = floating;
+
+    wordle_help(0, current, in, float_copy, dict, result);
+
+    return result;
+
+    // need to read in which has size/ letters in palce "-"
+    // helper fucntion that looks through in
+    //another that looks at the floating 
+    // once that is determined we can look through dictionary 
+
+    // if i do it this way will I i call a recurive fucntion for world itset?
 
 }
 
 // Define any helper functions here
+void wordle_help(
+    int index,
+    std::string& current,
+    const std::string& in,
+    std::string& float_left,
+    const std::set<std::string>& dict,
+    std::set<std::string>& result)
+    {
+      
+
+      // base cast checking that therrs no slots 
+      int slots = in.size() - index;
+      if ((int)float_left.size() > slots){
+        return;
+      }
+
+      // prune implemention 
+
+
+      //string current = in;
+      // base case  where there are no more flaoting words
+      //length has been met so that mesn a word has been created
+      // if word is created and exsits in dictioanry then add to set
+      if((int)in.size() == index){// compre ogioanl size with the word beign created 
+        if (float_left.empty() && dict.count(current) == 1 ){// or floating_left[index]==NULL
+           
+            result.insert(current);  
+          
+        }
+        return;
+      }
+
+      
+
+      //looking for fixed letter to add into the word
+      if (in[index] != '-'){
+        current[index]= in[index];
+        wordle_help(index+1, current, in, float_left, dict, result);
+        return;
+      }
+      
+      // try and find differen comobo of what potential word can go there
+      // focus just on floats  
+      //size_t float_size = float_left.size();
+      for (size_t i=0; float_left.size() > i; i++){
+
+        current[index]= float_left[i];
+
+        char use = float_left[i];
+        float_left[i] = float_left[float_left.size() -1];
+        float_left[float_left.size() -1] = use;
+        // string float_next = float_left;
+        // char removed = float_left[i];
+        // float_left.erase(i,1);
+        float_left.pop_back();
+        wordle_help(index+1, current, in, float_left, dict, result);
+        float_left.push_back(use);
+        float_left[float_left.size() -1] = float_left[i];
+        float_left[i] = use;
+          
+      } 
+
+      // any other letter in dicitonary 
+      //char c = 'a';// might have to move outside/ if not it will continue to reset
+      int remaining_slots = in.size() - index - 1;
+      if ((int) float_left.size() <= remaining_slots){
+        // as i loop will be finding possible combos
+        for (char c='a'; c <= 'z'; c++){
+          bool found = false;
+          for (size_t i=0; i < float_left.size(); i++){
+            if (float_left[i] == c){
+              found = true;
+              break;
+            }
+          }
+          if (!found){ 
+            current[index] = c;
+            wordle_help(index+1, current, in, float_left, dict, result);
+
+          }
+          
+        } 
+      } 
+      
+
+
+
+    }
